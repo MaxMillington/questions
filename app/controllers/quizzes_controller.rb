6b3b98efd316d6fa3334273a7bd4d455a98966a1
MaxMillington <maxmillington89@gmail.com>
2015-12-10 15:49:30 -0700

    fix bug that was breaking quizzes controller, changed it so that the homepage redirects to new_quiz_path

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,15 +1,22 @@
-class QuizzessController < ApplicationController
+class QuizzesController < ApplicationController
 
   def new
-
+    @quiz = Quiz.new
   end
 
   def index
-
+    @quizzes = Quiz.all
   end
 
   def create
+    @quiz = Quiz.new(quiz_params)
 
+    if @quiz.save
+      redirect_to quiz_path(@quiz)
+    else
+      redirect_to new_quiz_path
+      flash.keep[:danger] = 'You must enter an integer greater than 0.'
+    end
   end
 
   def update
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,7 @@
 class Quiz < ActiveRecord::Base
 
+  validates :size, :numericality => { :greater_than => 0 }
+  validates :size, presence: true
 
   has_many :questions
 end
